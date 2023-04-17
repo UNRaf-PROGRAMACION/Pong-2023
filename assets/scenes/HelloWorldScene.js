6d9d44f40996acaa77923496413ec257464fb770
Nicolas Nocete <nicolasnocete@gmail.com>
2023-04-17 15:07:21 -0300

    add comments in files

--- assets/scenes/HelloWorldScene.js
+++ assets/scenes/HelloWorldScene.js
@@ -1,5 +1,7 @@
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
+    // key of the scene
+    // the key will be used to start the scene by other scenes
     super("hello-world");
   }
 
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import HelloWorldScene from "./assets/scenes/HelloWorldScene.js";
 
-console.log("main.js");
-
+// Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -25,10 +24,11 @@ const config = {
       debug: true,
     },
   },
+  // List of scenes to load
+  // Only the first scene will be shown
+  // Remember to import the scene before adding it to the list
   scene: [HelloWorldScene],
 };
 
-console.log("main.js");
-console.log(config);
-
+// Create a new Phaser game instance
 window.game = new Phaser.Game(config);
