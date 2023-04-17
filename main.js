e7d2b83db71ec42669fef7e97122002f9fae6eb7
Nicolas Nocete <nicolasnocete@gmail.com>
2023-04-17 14:58:05 -0300

    create template files and folders

---
+++ main.js
@@ -0,0 +1,34 @@
+import HelloWorldScene from "./assets/scenes/HelloWorldScene.js";
+
+console.log("main.js");
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      width: 800,
+      height: 600,
+    },
+    max: {
+      width: 1600,
+      height: 1200,
+    },
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+      debug: true,
+    },
+  },
+  scene: [HelloWorldScene],
+};
+
+console.log("main.js");
+console.log(config);
+
+window.game = new Phaser.Game(config);
